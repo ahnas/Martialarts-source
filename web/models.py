c112b1822a177d63d05039fb26c2e74a681c1818
unknown <manuahnas@gmail.com>
2021-12-03 10:24:57 +0530

    model

--- web/models.py
+++ web/models.py
@@ -1,84 +1,93 @@
 from django.db import models
-from versatileimagefield.fields import VersatileImageField
+from versatileimagefield.fields import VersatileImageField,PPOIField
 
 # Create your models here.
 
 class Affiliation(models.Model):
-    name = models.CharField(max_length=128)
-    content = models.TextField()
+    name = models.CharField(max_length=128,blank=True,null=True)
+    content = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=128)
-    headname = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Referees/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    headname = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Referees/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class Referee(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Referees/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Referees/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+    
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class Judge(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Judges/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Judges/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 class Examinar(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Judges/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Judges/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 class Author(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Judges/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Judges/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class BlackBelt(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Judges/")
-    title = models.CharField(max_length=128)
-    titletwo = models.CharField(max_length=128)
-    titlethree = models.CharField(max_length=128)
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Judges/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
+    title = models.CharField(max_length=128,blank=True,null=True)
+    titletwo = models.CharField(max_length=128,blank=True,null=True)
+    titlethree = models.CharField(max_length=128,blank=True,null=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class Gallery(models.Model):
-    name = models.CharField(max_length=128)
-    photo = VersatileImageField('Photo',upload_to="Gallery/")
+    name = models.CharField(max_length=128,blank=True,null=True)
+    photo = VersatileImageField('Photo',upload_to="Gallery/",blank=True,null=True,ppoi_field="ppoi")
+    ppoi=PPOIField()
 
     def __str__(self):
         return str(self.name)
